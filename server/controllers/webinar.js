9c4a852bb62186bca046a4aff10455f8d5940b31
Nathan Waite <natew821@gmail.com>
2021-02-26 11:11:55 -0700

    Created Webinar Controller

---
+++ server/controllers/webinar.js
@@ -0,0 +1,37 @@
+const { REACT_APP_WEBINAR_KEY } = process.env;
+const fetch = require("node-fetch");
+
+module.exports = {
+  getWebinarInfo: async (req, res) => {
+    const ever_api = `https://api.webinarjam.com/everwebinar/webinar?api_key=${REACT_APP_WEBINAR_KEY}&webinar_id=57`;
+    const fetch_response = await fetch(ever_api, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const myJson = await fetch_response.json();
+    res.send(myJson);
+  },
+
+  registerUser: async (req, res) => {
+    const {
+      first_name,
+      last_name,
+      email,
+      phone,
+      webinar_id,
+      schedule,
+    } = req.params;
+    const api_url = `https://api.webinarjam.com/everwebinar/register?api_key=${REACT_APP_WEBINAR_KEY}&schedule=${schedule}&webinar_id=${webinar_id}&first_name=${first_name}&last_name=${last_name}&email=${email}&phone=${phone}`;
+    const fetch_user = await fetch(api_url, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const myJson = await fetch_user.json();
+    res.send(myJson);
+  },
+};
--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ const request = require("request");
 const express = require("express");
 const fetch = require("node-fetch");
 const session = require("express-session");
-const { SESSION_SECRET, REACT_APP_WEBINAR_KEY, SESSION_PORT } = process.env;
+const webinar = require("./controllers/webinar");
+const { SESSION_SECRET, SESSION_PORT } = process.env;
 
 const app = express();
 app.use(express.json());
@@ -22,43 +23,48 @@ app.use((req, res, next) => {
   next();
 });
 
-//Get Webinar Data//
-app.post("/api/webinar", async (req, res) => {
-  const ever_api = `https://api.webinarjam.com/everwebinar/webinar?api_key=${REACT_APP_WEBINAR_KEY}&webinar_id=56`;
-  const fetch_response = await fetch(ever_api, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  const myJson = await fetch_response.json();
-  res.send(myJson);
-});
+//Get EverWebinar Data//
+app.post("/api/webinar", webinar.getWebinarInfo);
+// app.post("/api/webinar", async (req, res) => {
+//   const ever_api = `https://api.webinarjam.com/everwebinar/webinar?api_key=${REACT_APP_WEBINAR_KEY}&webinar_id=57`;
+//   const fetch_response = await fetch(ever_api, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   });
+//   const myJson = await fetch_response.json();
+//   res.send(myJson);
+// });
 
-//Register User to Webinar  DONT FORGET TO ADD ASYNC AND AWAIT//
+//Register User to Webinar//
 app.post(
   "/api/webinar/register/:first_name/:last_name/:email/:phone/:webinar_id/:schedule/",
-  async (req, res) => {
-    const {
-      first_name,
-      last_name,
-      email,
-      phone,
-      webinar_id,
-      schedule,
-    } = req.params;
-    const api_url = `https://api.webinarjam.com/everwebinar/register?api_key=${REACT_APP_WEBINAR_KEY}&schedule=${schedule}&webinar_id=${webinar_id}&first_name=${first_name}&last_name=${last_name}&email=${email}&phone=${phone}`;
-    const fetch_user = await fetch(api_url, {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    const myJson = await fetch_user.json();
-    res.send(myJson);
-  }
+  webinar.registerUser
 );
+// app.post(
+//   "/api/webinar/register/:first_name/:last_name/:email/:phone/:webinar_id/:schedule/",
+//   async (req, res) => {
+//     const {
+//       first_name,
+//       last_name,
+//       email,
+//       phone,
+//       webinar_id,
+//       schedule,
+//     } = req.params;
+//     const api_url = `https://api.webinarjam.com/everwebinar/register?api_key=${REACT_APP_WEBINAR_KEY}&schedule=${schedule}&webinar_id=${webinar_id}&first_name=${first_name}&last_name=${last_name}&email=${email}&phone=${phone}`;
+//     const fetch_user = await fetch(api_url, {
+//       method: "post",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     });
+
+//     const myJson = await fetch_user.json();
+//     res.send(myJson);
+//   }
+// );
 
 app.listen(SESSION_PORT, () =>
   console.log(`Listening on port ${SESSION_PORT}`)
