c0dfba628bc1dec4efa2c8ea2850f1d7bec39c5c
Nathan Waite <natew821@gmail.com>
2021-04-01 20:07:32 -0600

    Pushing

--- server/index.js
+++ server/index.js
@@ -9,14 +9,16 @@ const { SESSION_SECRET, SESSION_PORT, CONNECTION_STRING } = process.env;
 const app = express();
 app.use(express.json());
 
-// app.use(
-//   session({
-//     resave: false,
-//     saveUninitialized: true,
-//     secret: SESSION_SECRET,
-//     cookie: { maxAge: 1000 * 60 * 60 * 24 },
-//   })
-// );
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+  })
+);
+
+app.use(express.static(`${__dirname}/../build`));
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
